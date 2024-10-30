db4089bee8e42d2c7e5e4e290ec020cdea9a1604
clueking2 <clairelueking@gmail.com>
2024-10-30 13:39:17 -0600

    added db_create route

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def testing():
     conn = psycopg2.connect("postgresql://renderlab_user:MnNSDQTXLLXHeWRKBSAfOSs3QdvKQfkD@dpg-csh8h9dumphs73c2ebr0-a/renderlab")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def table():
+    conn = psycopg2.connect("postgresql://renderlab_user:MnNSDQTXLLXHeWRKBSAfOSs3QdvKQfkD@dpg-csh8h9dumphs73c2ebr0-a/renderlab")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
