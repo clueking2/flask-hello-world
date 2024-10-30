b2d5e3a3baaccdf83eed303b9f05cc9fe75c964c
clueking2 <clairelueking@gmail.com>
2024-10-30 13:51:03 -0600

    routes added to app: db_insert, db_select, db_drop

--- app.py
+++ app.py
@@ -28,3 +28,50 @@ def table():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://renderlab_user:MnNSDQTXLLXHeWRKBSAfOSs3QdvKQfkD@dpg-csh8h9dumphs73c2ebr0-a/renderlab")
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+    Values
+    ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+    ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+    ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+    ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://renderlab_user:MnNSDQTXLLXHeWRKBSAfOSs3QdvKQfkD@dpg-csh8h9dumphs73c2ebr0-a/renderlab")
+    cur = conn.cursor()
+    cur.execute('''
+    SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.commit()
+    conn.close()
+    response_string = ""
+    response_string +="<table>"
+    for player in records:
+        response_string += "<tr>"
+        for info in player:
+            response_string += "<td>{}</td>"
+        response_string += "</tr>"
+    response_string += "</table>"
+    return response_string
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgresql://renderlab_user:MnNSDQTXLLXHeWRKBSAfOSs3QdvKQfkD@dpg-csh8h9dumphs73c2ebr0-a/renderlab")
+    cur = conn.cursor()
+    cur.execute('''
+    DROP TABLE Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
