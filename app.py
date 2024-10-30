9a9b7a4e6cd89bb8cf448e7d4382b3d48bd7610c
clueking2 <clairelueking@gmail.com>
2024-10-30 13:33:31 -0600

    added db_test route to app and psycopg2-binary to requirements

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Claire in 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://renderlab_user:MnNSDQTXLLXHeWRKBSAfOSs3QdvKQfkD@dpg-csh8h9dumphs73c2ebr0-a/renderlab")
+    conn.close()
+    return "Database Connection Successful"
